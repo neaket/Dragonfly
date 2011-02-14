b55eccdb13cd5db50c6848eed12b2e411788989c
neaket <neaket@gmail.com>
2011-02-13 19:10:16 -0500

    Initial Commit

---
+++ Dragonfly.Tests/Class1.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using NUnit.Framework;
+
+namespace Dragonfly.Tests.Models.Levels
+{
+    [TestFixture]
+    public class LevelTests
+    {
+        [Test]
+        public void Success()
+        {
+            Assert.AreEqual(true, true);
+
+        }
+    }
+}
